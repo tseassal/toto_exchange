5aefdaeb7c8e5188bc1e5f91ad682fd417d6ff16
theoseassal <35302827+theoseassal@users.noreply.github.com>
2020-02-26 18:43:20 +0100

    init

---
+++ src/main/java/com/toto/toto_exchange/TotoExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.toto.toto_exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotoExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotoExchangeApplication.class, args);
+	}
+
+}
